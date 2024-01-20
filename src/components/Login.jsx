142e1572f79c04e167197ba98fdb39a9dfb8fc25
devmev10 <ravitokas90@gmail.com>
2024-01-20 13:34:56 +0530

    update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,26 @@ import { useState, useContext } from "react";
 import UserContext from "../context/UserContext";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [userName, setUsername] = useState();
+  const [password, setPassword] = useState();
+  function handleSubmit() {}
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="username"
+        value={userName}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="username"
+        value={userName}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
-f;
