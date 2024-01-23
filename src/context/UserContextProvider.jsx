3ebd1fcdd43ef42bcac8422d2750ad463887a990
devmev10 <ravitokas90@gmail.com>
2024-01-23 15:16:34 +0530

    update UserContextProvider.jsx

--- src/context/UserContextProvider.jsx
+++ src/context/UserContextProvider.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   return (
+    // Setup context
     <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
